03191adf5bad36bef8abfffaf2f0a04dd08dd545
voidrak <112881704+voidrak@users.noreply.github.com>
2024-06-03 13:53:01 +0300

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
- 
+
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import LandingPage from "./components/HomePage/LandingPage";
@@ -19,7 +19,7 @@ const App = () => {
         </Route>
       </Routes>
     </BrowserRouter>
- 
- 
+  );
+};
 
 export default App;
