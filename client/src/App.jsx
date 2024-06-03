d106e3256233751df33de72cbd46b1708589df43
voidrak <112881704+voidrak@users.noreply.github.com>
2024-06-03 13:41:14 +0300

    add router and context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import LandingPage from "./components/HomePage/LandingPage";
+import SharedLayout from "./components/SharedLayout/SharedLayout";
 
 const App = () => {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          {/* <Route path="*" element={<ErrorPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BuyerRegistrationForm from "../components/Authentication/BuyerRegistrationForm";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <BuyerRegistrationForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
