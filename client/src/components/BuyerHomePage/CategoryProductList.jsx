7fb4d3771988967c9c805e1b015e10c9e8510c82
voidrak <112881704+voidrak@users.noreply.github.com>
2024-06-03 18:51:17 +0300

    update

---
+++ client/src/components/BuyerHomePage/CategoryProductList.jsx
@@ -0,0 +1,96 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ProductContext } from "../../contexts/ProductContext";
+import ProductCard from "../ProductCard";
+
+const CategoryProductList = ({
+  postPerPage,
+  currentPage,
+  categoryMainList,
+  setCategoryMainList,
+  sortOptionValue,
+}) => {
+  const { productData, currentCategory } = useContext(ProductContext);
+  const [sortedList, setSortedList] = useState(categoryMainList);
+  const [loadedProducts, setLoadedProducts] = useState(0);
+
+  const handleProductLoad = () => {
+    setLoadedProducts((prevCount) => prevCount + 1);
+  };
+
+  const lastPostIndex = currentPage * postPerPage;
+  const firstPostIndex = lastPostIndex - postPerPage;
+
+  useEffect(() => {
+    if (currentCategory != "ALL") {
+      const filteredList = productData.filter((val) =>
+        val.category.includes(currentCategory),
+      );
+
+      setCategoryMainList(filteredList);
+    } else {
+      setCategoryMainList(productData);
+    }
+  }, [currentCategory, productData]);
+
+  useEffect(() => {
+    setSortedList(categoryMainList);
+  }, [categoryMainList]);
+
+  useEffect(() => {
+    switch (sortOptionValue) {
+      case "default":
+        {
+          setSortedList(categoryMainList);
+        }
+        break;
+      case "AZ":
+        {
+          const sortedCategory = [...categoryMainList].sort((a, b) =>
+            a.itemNameEnglish.localeCompare(b.itemNameEnglish),
+          );
+          setSortedList(sortedCategory);
+        }
+        break;
+      case "ZA":
+        {
+          const sortedCategory = [...categoryMainList].sort((a, b) =>
+            b.itemNameEnglish.localeCompare(a.itemNameEnglish),
+          );
+          setSortedList(sortedCategory);
+        }
+        break;
+      case "toHigh":
+        {
+          const sortedCategory = [...categoryMainList].sort(
+            (a, b) => a.price - b.price,
+          );
+          setSortedList(sortedCategory);
+        }
+        break;
+      case "toLow":
+        {
+          const sortedCategory = [...categoryMainList].sort(
+            (a, b) => b.price - a.price,
+          );
+          setSortedList(sortedCategory);
+        }
+        break;
+      default:
+        setSortedList(categoryMainList);
+    }
+  }, [sortOptionValue, currentCategory]);
+
+  const mappedCategoryList = sortedList
+    .slice(firstPostIndex, lastPostIndex)
+    .map((product, index) => (
+      <ProductCard product={product} key={index} onLoad={handleProductLoad} />
+    ));
+
+  return (
+    <div className="mx-auto mt-8 grid max-w-[1280px] grid-cols-2 place-items-center gap-y-4 px-2 min-[481px]:px-4 min-[576px]:px-6 sm:w-[90%] sm:px-8 md:grid-cols-3 min-[931px]:max-w-[837px] min-[1200px]:max-w-[1024px] min-[1200px]:grid-cols-4 min-[1500px]:max-w-[1240px]">
+      {mappedCategoryList}
+    </div>
+  );
+};
+
+export default CategoryProductList;
--- client/src/components/SharedLayout/SharedLayout.jsx
+++ client/src/components/SharedLayout/SharedLayout.jsx
@@ -1,9 +1,12 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import CartSection from "./CartSection";
 const SharedLayout = () => {
+  const [isCart, setIsCart] = useState(true);
   return (
     <div>
       SharedLayout
+      <CartSection isCart={isCart} setIsCart={setIsCart} />
       <Outlet />
     </div>
   );
